b0ee6fc583d5dadfeb7e5ae1350a456f296a2b93
tricinel <bogdan@bogdanlazar.com>
2018-05-25 15:54:16 +0200

    Improve test coverage

---
+++ src/config/__tests__/eslint-rules.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable global-require */
+
+afterEach(() => {
+  jest.resetModules();
+});
+
+test('Gets the eslint rules when there is Flow as a project dependency', () => {
+  jest.mock('../../utils/hasDep', () => ({ hasDep: dep => dep === 'flow' }));
+  const config = require('../eslint-rules');
+  expect(config.rules['prettier/prettier'][1].parser).toEqual('flow');
+});
