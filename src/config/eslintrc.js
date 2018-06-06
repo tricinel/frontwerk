b87843a218a15417dc4dcaf58f04763107513844
tricinel <bogdan@bogdanlazar.com>
2018-06-06 13:28:39 +0200

    Update eslintrc to always use Jest plugins and rules

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -2,14 +2,13 @@ const { hasDep } = require('../utils/hasDep');
 const rules = require('./eslint-rules');
 
 const hasReact = hasDep('react');
-const hasJest = hasDep('jest');
 const hasJquery = hasDep('jquery');
 
 module.exports = {
   env: {
     browser: true,
     node: true,
-    jest: hasJest,
+    jest: true,
     jquery: hasJquery
   },
   parser: require.resolve('babel-eslint'),
@@ -18,11 +17,11 @@ module.exports = {
       ? require.resolve('eslint-config-airbnb')
       : require.resolve('eslint-config-airbnb-base'),
     require.resolve('eslint-config-prettier'),
-    hasJest ? 'plugin:jest/recommended' : null
+    'plugin:jest/recommended'
   ].filter(Boolean),
   plugins: [
     'prettier',
-    hasJest ? 'jest' : false,
+    'jest',
     hasReact ? 'import' : false,
     hasReact ? 'react' : false,
     hasReact ? 'jsx-a11y' : false
